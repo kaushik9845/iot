4e23778188f38ca0241afaff0a5b2ae7ea309236
kaushik9845 <kaushik9845@gmail.com>
2019-02-03 16:52:43 +0530

    Update Application.java
    adding servletinitializer

--- view/src/main/java/hello/Application.java
+++ view/src/main/java/hello/Application.java
@@ -20,7 +20,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
