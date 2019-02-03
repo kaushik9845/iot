36b226b574f3ad881a030354fb93e9c52b57bf2a
kaushik9845 <kaushik9845@gmail.com>
2019-02-03 16:55:20 +0530

    Update Application.java
    added import

--- view/src/main/java/hello/Application.java
+++ view/src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
