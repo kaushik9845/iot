a362c023818b57876c0408e83870b0b7b7e921c9
kaushik9845 <kaushik9845@gmail.com>
2019-02-04 09:45:08 +0530

    creating server within view project

---
+++ view/src/main/java/hello/AsyncClass.java
@@ -0,0 +1,66 @@
+package hello;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+@EnableAsync
+@Component
+public class AsyncClass {
+
+	@Async
+	public void socketserver() throws IOException, ClassNotFoundException
+	{
+		ServerSocket server;
+	    //socket server port on which it will listen
+	    int port = 6789;
+	    
+	    server = new ServerSocket(port);
+        //keep listens indefinitely until receives 'exit' call or program terminates
+        while(true){
+            System.out.println("Waiting for client request");
+            //creating socket and waiting for client connection
+            Socket socket = server.accept();
+            //read from socket to ObjectInputStream object
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            //convert ObjectInputStream object to String
+            String message = (String) ois.readObject();
+            System.out.println("Message Received: " + message);
+            //create ObjectOutputStream object
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            //write object to Socket
+            oos.writeObject("success");
+            //close resources
+            ois.close();
+            oos.close();
+            socket.close();
+            
+            
+            
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost("http://localhost:8080/view/rest/hello");
+            String JSON_STRING="";
+            HttpEntity stringEntity = new StringEntity(message,ContentType.DEFAULT_TEXT);
+            httpPost.setEntity(stringEntity);
+            CloseableHttpResponse response2 = httpclient.execute(httpPost);
+            System.out.println("Done making post call");
+            }
+            
+            //terminate the server if client sends exit request
+            //if(message.equalsIgnoreCase("exit")) break;
+        }
+	
+}
--- view/src/main/java/hello/WebSocketConfig.java
+++ view/src/main/java/hello/WebSocketConfig.java
@@ -1,5 +1,9 @@
 package hello;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -19,6 +23,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+    
+    @Autowired AsyncClass async;
+    @Bean 
+    public String test()
+    {
+    	try {
+			async.socketserver();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return "hi";
     }
 
 }
